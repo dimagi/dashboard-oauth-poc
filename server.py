b57a387b5f7cef7bdb7713d74436144c452ff55e
Cory Zue <czue@dimagi.com>
2020-07-23 13:48:21 +0200

    set auth tokens in cookies

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
-import json
-
 import requests
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, make_response
 from flask import render_template
 
-from commcare.auth import get_commcare_service, get_commcare_auth_config
+from commcare.auth import get_commcare_auth_config
 
 app = Flask(__name__)
 app.config.from_object('settings')
+
+ACCESS_TOKEN_COOKIE_NAME = 'commcare-access-token'
+REFRESH_TOKEN_COOKIE_NAME = 'commcare-refresh-token'
 
 @app.route('/')
 def home():
@@ -18,7 +19,6 @@ def home():
         bundle_url=bundle_url,
         commcare_config=commcare_config,
     )
-
 
 
 @app.route('/oauth-callback/')
@@ -36,19 +36,28 @@ def oauth_callback():
         'client_secret': auth_config['client_secret'],
         'redirect_uri': auth_config['redirect_uri'],
     }
-    print(json.dumps(post_data, indent=2))
-    resp = requests.post(
+    hq_response = requests.post(
         auth_config['access_token_url'],
         data=post_data,
     )
-    print(resp.json())
-    bundle_url = url_for('static', filename='index-bundle.js')
-    return render_template(
-        'hello.html',
-        bundle_url=bundle_url,
-        commcare_config=config,
-    )
-
+    if hq_response.status_code == 200:
+        hq_json = hq_response.json()
+        access_token = hq_json['access_token']
+        refresh_token = hq_json['refresh_token']
+        bundle_url = url_for('static', filename='index-bundle.js')
+        resp = make_response(
+            render_template(
+                'hello.html',
+                bundle_url=bundle_url,
+                commcare_config=config,
+        ))
+        # set cookies so they are available to the front end
+        resp.set_cookie(ACCESS_TOKEN_COOKIE_NAME, access_token)
+        resp.set_cookie(REFRESH_TOKEN_COOKIE_NAME, refresh_token)
+        return resp
+    else:
+        # todo: better handle error case and such
+        return "Oops, something went wrong. Please try again."
 
 def _get_commcare_config():
     return {k: v for k, v in app.config.items() if k.startswith('COMMCARE_')}
