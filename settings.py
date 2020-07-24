e32002836f5caba5d4e3af1325b0d16eee6a5fcb
Cory Zue <czue@dimagi.com>
2020-07-24 09:03:07 +0200

    commit settings file

---
+++ settings.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+COMMCARE_CLIENT_ID = os.environ.get('COMMCARE_CLIENT_ID', '')
+COMMCARE_CLIENT_SECRET = os.environ.get('COMMCARE_CLIENT_SECRET', '')
+COMMCARE_REDIRECT_URI = os.environ.get('COMMCARE_REDIRECT_URI', '')
+COMMCARE_AUTH_TOKEN = os.environ.get('COMMCARE_AUTH_TOKEN', '')
+COMMCARE_URL = os.environ.get('COMMCARE_URL', '')
+COMMCARE_DEFAULT_USERNAME = os.environ.get('COMMCARE_DEFAULT_USERNAME', '')
+COMMCARE_DEFAULT_API_KEY = os.environ.get('COMMCARE_DEFAULT_API_KEY', '')
+
+COMMCARE_DEFAULT_API  = os.environ.get('COMMCARE_DEFAULT_API', '')
+
+COMMCARE_DOMAIN = os.environ.get('COMMCARE_DOMAIN', '')
+COMMCARE_AGGREGATE_REPORT = os.environ.get('COMMCARE_AGGREGATE_REPORT', '')
+COMMCARE_LIST_REPORT = os.environ.get('COMMCARE_LIST_REPORT', '')
+COMMCARE_DEV_MODE = os.environ.get('COMMCARE_DEV_MODE', '')
