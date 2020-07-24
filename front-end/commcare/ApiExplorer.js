b8e52dfff6462247631ea696dde6345a8ce0b887
Cory Zue <czue@dimagi.com>
2020-07-24 09:16:51 +0200

    remove api key authorization option

--- front-end/commcare/ApiExplorer.js
+++ front-end/commcare/ApiExplorer.js
@@ -6,10 +6,7 @@ function ApiExplorer(props) {
   const [api, setApi] = useState(props.config.COMMCARE_DEFAULT_API);
   const [apiData, setApiData] = useState('')
   const [isLoading, setIsLoading] = useState(false);
-  const authorization = props.authToken ?
-    getOAuth2TokenAuthorization(props.authToken) :
-    getAPIKeyAuthorization(props.username, props.apiKey);
-  console.log(authorization);
+  const authorization = getOAuth2TokenAuthorization(props.authToken);
   const hitApi = function () {
     setIsLoading(true);
     fetchCommCareApi(
@@ -26,11 +23,9 @@ function ApiExplorer(props) {
       }
     );
   };
-  const debug = <p>Your username is <strong>{props.username}</strong> and your api key is <strong>{props.apiKey}</strong></p>;
   return (
     <div className="ApiExplorer">
       <h1>CommCare API Explorer</h1>
-      {props.devMode ? debug : ''}
       <h2>API Url</h2>
       <input type="text" style={{width: "60em"}} value={api} onChange={(event) => setApi(event.target.value)}/>
       <br />
