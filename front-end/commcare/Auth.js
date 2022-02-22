0148261bbc3dce98d2c25ad63a6e7f06d7fee098
Cory Zue <cory@coryzue.com>
2022-02-22 14:08:35 +0200

    remove unused (and never available) client secret from front end

--- front-end/commcare/Auth.js
+++ front-end/commcare/Auth.js
@@ -8,12 +8,10 @@ import * as qs from "qs";
 
 export function getOAuthClient(config) {
   const clientId = config.COMMCARE_CLIENT_ID;
-  const clientSecret = config.COMMCARE_CLIENT_SECRET;
   const redirectURI = config.COMMCARE_REDIRECT_URI;
   const hqUrl = config.COMMCARE_URL;
   const auth = new ClientOAuth2({
     clientId: clientId,
-    clientSecret: clientSecret,
     accessTokenUri: `${hqUrl}/oauth/token/`,
     authorizationUri: `${hqUrl}/oauth/authorize/`,
     redirectUri: redirectURI,
